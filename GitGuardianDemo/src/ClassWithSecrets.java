80eaebb4f5fda3c8f9f1c6d9335949502ac9a824
Vigneshram <vigneshram001@gmail.com>
2022-11-22 18:04:04 +0530

    First Commit

---
+++ GitGuardianDemo/src/ClassWithSecrets.java
@@ -0,0 +1,13 @@
+
+public class ClassWithSecrets {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		String username = "testtest@gmail.com";
+		String password = "Password@123";
+		
+	}
+
+}
